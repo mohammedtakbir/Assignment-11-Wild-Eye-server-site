1ff3eb88e7ad137c5a692dfafb28c4bd3bda3eb2
mohammedtakbir <takbirhossain678@gmail.com>
2022-11-09 10:57:18 +0600

    create service-details api and get review data from client

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
@@ -20,20 +20,36 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 console.log(uri)
 async function run() {
     const servicesCollection = client.db('wildEye').collection('services');
+    const reviewsCollection = client.db('wildEye').collection('reviews');
     try {
 
-        app.get('/services', async(req, res) => {
+        app.get('/services', async (req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
 
-        app.get('/services/home', async(req, res) => {
+        app.get('/services/home', async (req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.limit(3).toArray();
             res.send(services)
+        })
+
+        app.get('/service-details/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+
+        //* reviews
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result);
         })
 
     }
